d40b2a0185bfb868436c084a90baf9c505aeff29
mlrainer <112909069+mlrainer@users.noreply.github.com>
2023-03-28 21:14:37 -0400

    Minor changes for the day

--- LegoSetInventoryManager/Controllers/LegoSetController.cs
+++ LegoSetInventoryManager/Controllers/LegoSetController.cs
@@ -1,11 +1,71 @@
 ﻿using System;
+using LegoSetInventoryManager.Data;
+using LegoSets;
+using Microsoft.AspNetCore.Mvc;
+
 namespace LegoSetInventoryManager.Controllers
 {
-	public class LegoSetController
-	{
-		public LegoSetController()
-		{
-		}
-	}
-}
+    public class LegoSetController : Controller
+    {
+        private DBContext _context;
+        public LegoSetController()
 
+        {
+
+            _context = new DBContext();
+
+        }
+
+
+        protected override void Dispose(bool disposing) => _context.Dispose();
+
+        [Route("SetName/Index")]
+
+        public ActionResult Index()
+
+        {
+
+            var SetList = _context.SetName;
+
+            return View("Index", SetList);
+
+        }
+
+        public ActionResult New()
+
+        {
+
+            var set = new LegoSets.SetName();
+
+            return View("SetForm", set);
+
+        }
+
+        public ActionResult Save(SetName set)
+
+        {
+
+            if (!ModelState.IsValid)
+
+            {
+
+                var _set = set;
+
+
+                return View("SetForm", set);
+
+            }
+
+
+            _context.SetName.Add(set);
+
+
+            _context.SaveChanges();
+
+
+            return RedirectToAction("Index", "Set Name");
+
+        }
+    }
+
+}
